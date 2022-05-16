c19b99391f63356995c4ff05feadd3405d491227
Fabio Miranda <elolmuski@gmail.com>
2022-05-16 14:37:05 -0300

    first commit

---
+++ orders.js
@@ -0,0 +1,47 @@
+const Orders = [
+    {
+
+        productName: 'Foldable Mini Drone',
+        productNumber: '85631',
+        paymentStatus: 'Due',
+        shipping: 'Pending',
+    },
+    {
+
+        productName: 'LARVENDER KF102 Drone',
+        productNumber: '36378',
+        paymentStatus: 'Refunded',
+        shipping: 'Decline'
+    },
+    {
+
+        productName: 'Ruko F11 Pro Drone',
+        productNumber: '49347',
+        paymentStatus: 'Due',
+        shipping: 'Pending',
+    },
+    {
+        productName: 'Drone with Camera',
+        productNumber: '96996',
+        paymentStatus: 'Paid',
+        shipping: 'Delivired',
+    },
+    {
+        productName: 'GPS 4k Drone',
+        productNumber: '22821',
+        paymentStatus: 'Paid',
+        shipping: 'Delivired'
+    },
+    {
+        productName: 'DJI Air 2S',
+        productNumber: '81475',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Lazenge Drone',
+        productNumber: '00482',
+        paymentStatus: 'Paid',
+        shipping: 'Delivired'
+    },
+]
---
+++ readme.md
@@ -0,0 +1,24 @@
+MEU PRIMEIRO DASHBOARD
+
+INTRODUÇÃO
+-----------------------------------------------------------------------------------------------------------------------------------------
+
+Estarei comentando sobre coisas novas que usarei nessa aplicação. (Bem simples!)
+
+Começando pelo menu!
+
+O primeiro passo da aplicação será o menu 
+
+Google fonts
+Usando diversas funcionalidade do google fonts. A principal por enquanto é a utlização de icones para ilustração de atalhos.
+Material Icons -> https://fonts.sandbox.google.com/css2?family=Material+Symbols+Sharp:opsz,wght,FILL,GRAD@48,400,0,0
+
+A fonte usada é a Poppins com as configurações:
+light, regular, medium, semibold, bold e extrabold
+
+Poppins -> @import url('https://fonts.googleapis.com/css2?family=Koulen&family=Poppins:wght@300;400;500;600;700;800&display=swap');
+
+CSS
+Para o css um conjunto de variáveis para ser a raiz da aplicação com foco no estilo geral e em paletas de cores.
+
+1:22:00
